86794732cff5293f5aa4130096e22eb2afeca9d7
ArcticRaven <42956845+ArticRaven@users.noreply.github.com>
2019-03-25 16:18:25 -0500

    Add files via upload

---
+++ Lab9/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab9.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            Object a = null;
+            a.ToString();
+
+        }
+    }
+}
